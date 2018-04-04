2fb59be31c9bc807f48ac4c6278486f773eb1fbd
Marc-Aurel Audoin <marc-aurel.audoin@myco.coop>
2018-04-04 19:36:44 +0200

    Add remote debugger

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,12 @@ ENV PYTHONUNBUFFERED 1
 
 COPY . /app
 
+RUN pip install ptvsd==3.0.0
 RUN pip install -r /app/requirements.txt
 
 WORKDIR /app/project
 
 EXPOSE 8000
+EXPOSE 8001
 
 CMD python manage.py runserver 0.0.0.0:8000
--- project/routing.py
+++ project/routing.py
@@ -6,3 +6,15 @@ application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
         URLRouter(chat.routing.websocket_urlpatterns)),
 })
+
+try:
+    import ptvsd
+except:
+    pass
+else:
+    import logging
+    logger = logging.getLogger('daphne.server')
+    port = 8001
+    secret = "channels"
+    ptvsd.enable_attach(secret=secret, address=('0.0.0.0', port))
+    logger.info('Debugger attached (port={}, secret={})'.format(port, secret))
